260b60c7f01814e3d6570f42c0b4fe9482a7a7da
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-04-22 11:40:45 -0700

    Grid layout working

--- digital-resume/src/App.svelte
+++ digital-resume/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { writable } from "svelte/store";
-  import "./tailwind.css"
+  import "./tailwind.css";
+  import { crossfade, scale } from "svelte/transition";
 
   /* SECTIONS:
 
@@ -17,60 +18,100 @@
   Services
   */
 
-  const expandingSection = "text-dark-white hover:text-white hover:border-3 hover:border-3 transition-all text-lg border border-b-white p-2"
-  const activeSectionTw = (section: Section) => {
-    const sectionDisplay: Record<Section, string> = {
+  const [send, receive] = crossfade({
+    duration: 600,
+    fallback: scale,
+  });
+
+  type Section = "about" | "experience" | "projects" | "skillset" | "contact";
+
+  const sectionHeader = "text-xl font-bold mb-2";
+
+  const expandingSectionTw = (section?: Section) => {
+    const sectionOverrides: Partial<Record<Section, string>> = {
+      experience: "col-span-3",
+      projects: "col-span-full",
+    };
+
+    return (
+      "text-dark-white hover:text-white hover:border-3 hover:border-3 transition-all text-lg border border-b-white rounded p-2 " +
+      (section ? sectionOverrides[section] : "")
+    );
+  };
+
+  const activeSectionTw = (section?: Section) => {
+    const sectionOverrides: Partial<Record<Section, string>> = {
       experience: "col-span-3 row-span-3",
-      about: "col-span-3"
-    }
-    return "text-white border-3 shadow-md " + sectionDisplay[section]
+      about: "col-span-2 row-span-2",
+      skillset: "col-span-2 row-span-2",
+      contact: "row-start-2 col-start-2 col-span-2 row-span-2",
+      projects: "",
+    };
 
-  } 
-  const sectionHeader = "text-xl font-bold mb-2"
+    return "text-white border-3 shadow-md " + section
+      ? sectionOverrides[section]
+      : "";
+  };
 
-  type Section = "about" | "experience" | "projects" | "skillset" | "contact"
-
-  const activeSection =  writable<null | Section>(null)
-  $: toggleSection = (section: Section) => $activeSection === section ? activeSection.set(null) : activeSection.set(section)
-  $: isActive = (section: Section) => $activeSection == section
+  const activeSection = writable<null | Section>(null);
+  $: toggleSection = (section: Section) =>
+    $activeSection === section
+      ? activeSection.set(null)
+      : activeSection.set(section);
+  $: isActive = (section: Section) => $activeSection == section;
 </script>
 
-<div class="min-h-screen bg-gray-800 flex items-center px-[10vw] justify-center">
-  <div class="bg-gray-900 border-4  border-red p-6 rounded-lg shadow-lg">
+<div
+  class="flex min-h-screen items-center justify-center bg-gray-800 px-[10vw] sm:px-0"
+>
+  <div class="rounded-lg border-4 bg-gray-900 p-6 shadow-lg">
     <div class="grid grid-cols-3 gap-4">
-      <div class="col-span-3 text-center text-white text-4xl font-bold mb-4 border border-b-white p-2">
+      <div
+        class="col-span-3 mb-4 rounded border border-b-white p-2 text-center text-4xl font-bold text-white"
+      >
         Jackson Ernst
-        <h2 class="col-span-3 text-center font-thin text-lg">
+        <h2 class="col-span-3 text-center text-lg font-thin">
           Full Stack Web3 Developer and Protocol Engineer
         </h2>
       </div>
-      <div class={expandingSection + " " + (isActive("about") ? activeSectionTw("about") : "")}>
-        <button on:click|preventDefault={() => toggleSection("about")}>
-          <h2 class={sectionHeader}>About Myself</h2>
-          
-        </button>
-        
-        {#if isActive("about")}
-          <div>
-            More info about myself
-            More info about myself
-          </div>
-        {/if}
-      </div>
-      <div class={expandingSection + " " + (isActive("skillset") ? activeSectionTw("skillset") : "")}>
+
+      <!-- About Section -->
+      {#if !isActive("about")}
+        <div class={expandingSectionTw()}>
+          <button on:click|preventDefault={() => toggleSection("about")}>
+            <h2 class={sectionHeader}>About</h2>
+          </button>
+        </div>
+      {:else}
+        <div class={expandingSectionTw() + " " + activeSectionTw("about")}>
+          <button on:click|preventDefault={() => toggleSection("about")}>
+            <h2 class={sectionHeader}>About</h2>
+          </button>
+          <div>More info about myself More info about myself</div>
+        </div>
+      {/if}
+
+      <!-- Skillset Section -->
+      <div
+        class={expandingSectionTw() +
+          " " +
+          (isActive("skillset") ? activeSectionTw("skillset") : "")}
+      >
         <button on:click|preventDefault={() => toggleSection("skillset")}>
           <h2 class={sectionHeader}>Skillset</h2>
-          
         </button>
-        
+
         {#if isActive("skillset")}
-          <div>
-           Typescript 
-           Solidity
-          </div>
+          <div>Typescript Solidity</div>
         {/if}
       </div>
-      <div class={expandingSection  + " " + (isActive("contact") ? ("contact") : "")}>
+
+      <!-- Contact Section -->
+      <div
+        class={expandingSectionTw() +
+          " " +
+          (isActive("contact") ? activeSectionTw("contact") : "")}
+      >
         <button on:click|preventDefault={() => toggleSection("contact")}>
           <h2 class={sectionHeader}>Contact</h2>
         </button>
@@ -83,35 +124,68 @@
           </div>
         {/if}
       </div>
-      <div class={expandingSection  + " " + (isActive("projects") ? activeSectionTw("projects") : "")}>
+
+      <!-- Projects Section -->
+      <div
+        class={expandingSectionTw("projects") +
+          " " +
+          (isActive("projects") ? activeSectionTw("projects") : "")}
+      >
         <button on:click|preventDefault={() => toggleSection("projects")}>
           <h2 class={sectionHeader}>Projects</h2>
         </button>
         {#if isActive("projects")}
           <div>
-            <p>Full stack developer with 5+ years of experience in developing web applications using modern technologies. Proficient in JavaScript, React, Node.js, and MongoDB.</p>
+            <p>
+              Full stack developer with 5+ years of experience in developing web
+              applications using modern technologies. Proficient in JavaScript,
+              React, Node.js, and MongoDB.
+            </p>
           </div>
         {/if}
       </div>
-      <div class={expandingSection   + " " + (isActive("experience") ? activeSectionTw("experience") : "")}>
+
+      <!-- Experience Section -->
+      <div
+        class={expandingSectionTw("experience") +
+          " " +
+          (isActive("experience") ? activeSectionTw("experience") : "")}
+      >
         <button on:click|preventDefault={() => toggleSection("experience")}>
           <h2 class={sectionHeader}>Professional Experience</h2>
         </button>
         {#if isActive("experience")}
           <div>
-            <h3 class="text-lg font-bold">Full Stack Developer - XYZ Company</h3>
+            <h3 class="text-lg font-bold">
+              Full Stack Developer - XYZ Company
+            </h3>
             <p>Jan 2018 - Present</p>
-            <ul class="list-disc list-inside mb-4">
-              <li>Developed and maintained web applications using React and Node.js.</li>
+            <ul class="mb-4 list-inside list-disc">
+              <li>
+                Developed and maintained web applications using React and
+                Node.js.
+              </li>
               <li>Implemented RESTful APIs using Express.js and MongoDB.</li>
-              <li>Collaborated with cross-functional teams to deliver high-quality software.</li>
+              <li>
+                Collaborated with cross-functional teams to deliver high-quality
+                software.
+              </li>
             </ul>
             <h3 class="text-lg font-bold">Web Developer - ABC Company</h3>
             <p>Jun 2016 - Dec 2017</p>
-            <ul class="list-disc list-inside">
-              <li>Developed and maintained the company website using HTML, CSS, and JavaScript.</li>
-              <li>Implemented responsive design to ensure optimal user experience on all devices.</li>
-              <li>Collaborated with the marketing team to create engaging content for the website.</li>
+            <ul class="list-inside list-disc">
+              <li>
+                Developed and maintained the company website using HTML, CSS,
+                and JavaScript.
+              </li>
+              <li>
+                Implemented responsive design to ensure optimal user experience
+                on all devices.
+              </li>
+              <li>
+                Collaborated with the marketing team to create engaging content
+                for the website.
+              </li>
             </ul>
           </div>
         {/if}
