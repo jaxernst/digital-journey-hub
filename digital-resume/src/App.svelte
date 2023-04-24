00870ea4e61112585e1abc9fe0ec7038d2d08a49
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-04-23 22:37:05 -0700

    More improvements and updates

--- digital-resume/src/App.svelte
+++ digital-resume/src/App.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
+  import TwitterLogo from "./assets/twitter-logo.svelte";
+  import GithubLogo from "./assets/github-logo.svelte";
+  import LinkedinLogo from "./assets/linkedin-logo.svelte";
   import { derived, writable } from "svelte/store";
+
   import "./tailwind.css";
+
   import { crossfade, scale, fade, slide } from "svelte/transition";
+  import { SvelteToast, toast } from "@zerodevx/svelte-toast";
+  import { clickToCopyAction } from "svelte-legos";
+  import { Slidy } from "svelte-slidy";
   import {
     cubicInOut,
     bounceIn,
@@ -9,7 +17,9 @@
     elasticOut,
     circInOut,
     sineInOut,
+    cubicOut,
   } from "svelte/easing";
+  import EmailLogo from "./assets/email-logo.svelte";
 
   /* SECTIONS:
 
@@ -27,6 +37,7 @@
   */
 
   const sectionStyle = " bg-gray-900 rounded border border-b-white p-2 ";
+  const sectionHeader = "text-lg font-bold mb-2";
   const activeSectionTw = " z-10 col-span-3 ";
 
   const durationMain = 450;
@@ -36,30 +47,56 @@
     fallback: scale,
   });
 
-  type Section = "about" | "experience" | "projects" | "skillset" | "contact";
-
-  const sectionHeader = "text-xl font-bold mb-2";
+  type Section = "about" | "skillset" | "contact" | "experience" | "projects";
 
   const activeSection = writable<null | Section>(null);
   $: toggleSection = (section: Section) =>
     $activeSection === section
       ? activeSection.set(null)
       : activeSection.set(section);
+
   $: isActive = (section: Section) => $activeSection == section;
 </script>
 
+<SvelteToast />
+
 <div
-  class="flex min-h-screen items-center justify-center bg-gray-800 px-[10vw] sm:px-0"
+  class="flex min-h-screen items-center justify-center bg-gray-800 px-[10vw] md:px-0"
 >
   <div class="rounded-lg border-4 bg-gray-900 p-6 text-left shadow-lg">
     <div
-      class="col-span-3 mb-4 rounded border border-b-white p-4 text-center text-4xl font-bold text-white"
+      class="col-span-3 border-b-white text-center text-4xl font-bold text-white"
     >
       Jackson Ernst
       <h2 class="col-span-3 text-center text-lg font-thin">
-        Full Stack Web3 Developer and Protocol Engineer
+        Full Stack Web3 Developer
       </h2>
     </div>
+
+    <!-- Links/Icons -->
+    <div class="custom-grid place-items-center py-4 pb-6">
+      <div />
+      <div class="flex place-items-center gap-4">
+        <a href="https://github.com/jaxernst" class="h-6 w-6"><GithubLogo /></a>
+        <a href="https://twitter.com/yachtyyachty" class="h-6 w-6"
+          ><TwitterLogo /></a
+        >
+        <a
+          href="https://www.linkedin.com/in/jackson-ernst-9ab68014a/"
+          class="h-6 w-6"><LinkedinLogo /></a
+        >
+        <button
+          class="h-8 w-8"
+          use:clickToCopyAction={"jaxernst@gmail.com"}
+          on:click={() => toast.push("jaxernst@gmail.com copied to clipboard!")}
+        >
+          <EmailLogo />
+        </button>
+      </div>
+      <div />
+    </div>
+
+    <!-- Card Sections -->
     <div class="grid grid-cols-3 gap-4">
       <!-- About Section -->
       {#key $activeSection}
@@ -71,7 +108,7 @@
             (isActive("about") ? activeSectionTw : " text-dark-white ")}
         >
           <button on:click={() => toggleSection("about")}>
-            <h2 class="mb-2 text-xl font-bold">About</h2>
+            <h2 class={sectionHeader}>About</h2>
           </button>
 
           {#if isActive("about")}
@@ -103,7 +140,7 @@
               : "col-start-2 text-dark-white")}
         >
           <button on:click={() => toggleSection("skillset")}>
-            <h2 class="mb-2 text-xl font-bold">Skillset</h2>
+            <h2 class={sectionHeader}>Skillset</h2>
           </button>
 
           {#if isActive("skillset")}
@@ -135,7 +172,7 @@
               : "col-start-3 text-dark-white")}
         >
           <button on:click={() => toggleSection("contact")}>
-            <h2 class="mb-2 text-xl font-bold">Contact</h2>
+            <h2 class={sectionHeader}>Contact</h2>
           </button>
 
           {#if isActive("contact")}
@@ -158,28 +195,21 @@
         </div>
       {/key}
 
-      <!-- Projects Section -->
-      <div class={"col-span-full text-dark-white " + sectionStyle}>
-        <button on:click|preventDefault={() => toggleSection("projects")}>
-          <h2 class={sectionHeader}>Projects</h2>
-        </button>
-        {#if isActive("projects")}
-          <div>
-            <p>
-              Full stack developer with 5+ years of experience in developing web
-              applications using modern technologies. Proficient in JavaScript,
-              React, Node.js, and MongoDB.
-            </p>
-          </div>
-        {/if}
-      </div>
-
       <!-- Experience Section -->
       {#key $activeSection}
-        <div class={"col-span-full row-start-2 text-dark-white" + sectionStyle}>
-          <button on:click|preventDefault={() => toggleSection("experience")}>
+        <div
+          in:receive={{ key: "experience" }}
+          out:send={{ key: "experience" }}
+          class={"row-start-2 " +
+            sectionStyle +
+            (isActive("experience")
+              ? "z-10 col-span-3 col-start-1 row-start-1 bg-gray-900 shadow-lg "
+              : "col-start-1 text-dark-white")}
+        >
+          <button on:click={() => toggleSection("experience")}>
             <h2 class={sectionHeader}>Professional Experience</h2>
           </button>
+
           {#if isActive("experience")}
             <div transition:slide>
               <h3 class="text-lg font-bold">
@@ -217,6 +247,56 @@
           {/if}
         </div>
       {/key}
+
+      {#key $activeSection}
+        <div
+          in:receive={{ key: "projects" }}
+          out:send={{ key: "projects" }}
+          class={"row-start-2 " +
+            sectionStyle +
+            (isActive("projects")
+              ? "z-10 col-span-3 col-start-1 row-start-1 bg-gray-900 shadow-lg "
+              : "col-start-2 text-dark-white")}
+        >
+          <button on:click={() => toggleSection("projects")}>
+            <h2 class={sectionHeader}>Projects</h2>
+          </button>
+
+          {#if isActive("projects")}
+            <div transition:slide>
+              <div class="border border-dark-white" />
+              <h2>Projects</h2>
+              <div class="border border-dark-white" />
+              <h2>Projects</h2>
+              <div class="border border-dark-white" />
+              <h2>Projects</h2>
+              <div class="border border-dark-white" />
+            </div>
+          {/if}
+        </div>
+      {/key}
+    </div>
+
+    <h1 class="py-4 pt-6 text-lg font-bold">Featured</h1>
+
+    <!-- Highlights Section -->
+    <div
+      style="height:130px; width:250px"
+      class="relative m-2 overflow-hidden rounded-2xl border border-dark-white"
+    >
+      <video autoplay loop alt={""} src={"src/assets/boid-demo.mp4"} />
+      <div
+        class="bg-dark-white-trans absolute bottom-0 left-0 h-[19%] w-full p-0 pl-2 text-sm font-bold text-white"
+      >
+        Boids: Interactive Flocking
+      </div>
     </div>
   </div>
 </div>
+
+<style>
+  .custom-grid {
+    display: grid;
+    grid-template-columns: 1fr minmax(0, 200px) 1fr;
+  }
+</style>
